a11ea94f3e763f4354b00169dd22edc29c0b8875
cozmus <cihangir.ozmus@booking.com>
2026-07-20 11:44:32 +0200

    mutex is added?

--- order-process/main.go
+++ order-process/main.go
@@ -10,6 +10,7 @@ import (
 type Order struct {
 	Id     int
 	Status OrderStatus
+	mu     sync.Mutex
 }
 
 type OrderStatus string
@@ -23,35 +24,45 @@ var Statuses = []OrderStatus{
 	"Complete",
 }
 
+var (
+	totalUpdates int
+	updateMutex  sync.Mutex
+)
+
 func main() {
 	orders := generateOrders(10)
 
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(4)
 	go func() {
 		defer wg.Done()
 		processOrders(orders)
 	}()
-	go func() {
-		defer wg.Done()
-		updateOrderStatuses(orders)
-	}()
 
-	go func() {
-		defer wg.Done()
-		reportOrdersStatus(orders)
-	}()
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+
+				updateOrderStatuses(order)
+			}
+		}()
+	}
 	wg.Wait()
 
+	reportOrdersStatus(orders)
 	fmt.Println("All orders are processed!")
+	fmt.Println("Total updates processed:", totalUpdates)
 }
 
-func updateOrderStatuses(orders []*Order) {
-	for _, order := range orders {
-		time.Sleep(1 * time.Second)
-		order.Status = Statuses[rand.Intn(len(Statuses))]
-		fmt.Printf("Updating Order <%d> status is:  <%s>\n", order.Id, order.Status)
-	}
+func updateOrderStatuses(order *Order) {
+	time.Sleep(1 * time.Second)
+	order.Status = Statuses[rand.Intn(len(Statuses))]
+	fmt.Printf("Updating Order <%d> status is:  <%s>\n", order.Id, order.Status)
+
+	currentUpdates := totalUpdates
+	time.Sleep(5 * time.Millisecond)
+	totalUpdates = currentUpdates + 1
 }
 
 func processOrders(orders []*Order) {
@@ -71,10 +82,8 @@ func generateOrders(count int) []*Order {
 }
 
 func reportOrdersStatus(orders []*Order) {
-	for i := 0; i < 3; i++ {
-		for _, order := range orders {
-			time.Sleep(1 * time.Second)
-			fmt.Printf("Reporting Order <%d> status is:  <%s>\n", order.Id, order.Status)
-		}
+	for _, order := range orders {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Reporting Order <%d> status is:  <%s>\n", order.Id, order.Status)
 	}
 }
